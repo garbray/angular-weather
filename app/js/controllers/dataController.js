15652d96eb6f514ffaa88ae18d5d3eb457693387
Bryan Garzon <bryangarzon@BryanGarzon.local>
2013-10-23 15:21:32 -0500

    update files

--- app/js/controllers/dataController.js
+++ app/js/controllers/dataController.js
@@ -1,4 +1,7 @@
-appWeather.controller('dataController', function($scope, position) {
+appWeather.controller('dataController', function($scope, position, getTime) {
+
+	console.log(getTime);
+
 	position.getCurrentPosition(function showPosition(poss){
 		$scope.temp = { 'latitude': poss.coords.latitude, 'Longitude': poss.coords.longitude};
 		console.log($scope);
